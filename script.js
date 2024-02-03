53a470ff4cf68efcc675c330f129bb18093223b2
Sujal Thapa <suzaalproff@gmail.com>
2024-02-03 14:27:31 -0500

    Trying to put smooth scrolling so it is very rough right now

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function darkmode() {
   const element = document.body;
   element.classList.toggle("dark-mode");
-
+}
+function scrollSection(sectionId){
+  const section = document.querySelector(sectionId);
+  section.scrollSection({behavior: 'smooth'});
 }
